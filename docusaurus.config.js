bd25550feefa7661c86f6118590e2502884a1046
Xing Yahao <keidarcy.1015@gmail.com>
2021-10-09 15:01:06 +0900

    chore: add global style

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,19 +1,25 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const title = 'Typescript Type Challenges List';
+
+const lowerCaseTitle = title.toLocaleLowerCase().replace(/\s/g, '-');
+
+const githubUrl = 'https://github.com/keidarcy/' + lowerCaseTitle;
+
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (
   module.exports = {
-    title: 'Typescript Type Challenge List',
-    tagline: 'Dinosaurs are cool',
-    url: 'https://your-docusaurus-test-site.com',
+    title,
+    tagline: 'Typescript types are cool',
+    url: `https://${lowerCaseTitle}.vercel.app/`,
     baseUrl: '/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: 'keidarcy',
+    projectName: lowerCaseTitle,
 
     presets: [
       [
@@ -25,7 +31,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             // Please change this to your repo.
             path: './docs/',
             routeBasePath: '/',
-            editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+            editUrl: `${githubUrl}/blob/master`,
           },
           theme: {
             customCss: require.resolve('./src/css/custom.css'),
@@ -45,20 +51,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
       /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
         navbar: {
-          title: 'Typescript Type Challenge List',
+          title,
           logo: {
-            alt: 'My Site Logo',
+            alt: 'Dark Ts Logo',
             src: 'img/logo.svg',
           },
           items: [
-            // {
-            //   type: 'doc',
-            //   docId: 'intro',
-            //   position: 'left',
-            //   label: 'Tutorial',
-            // },
             {
-              href: 'https://github.com/facebook/docusaurus',
+              href: githubUrl,
               label: 'GitHub',
               position: 'right',
             },
@@ -68,7 +68,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
           style: 'dark',
           links: [
             {
-              title: 'Type Challenges',
+              title: 'Inspired by',
               items: [
                 {
                   label: 'type-challenges',
@@ -77,37 +77,25 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
               ],
             },
             {
-              title: 'Community',
-              items: [
-                {
-                  label: 'Stack Overflow',
-                  href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                },
-                {
-                  label: 'Discord',
-                  href: 'https://discordapp.com/invite/docusaurus',
-                },
-                {
-                  label: 'Twitter',
-                  href: 'https://twitter.com/docusaurus',
-                },
-              ],
-            },
-            {
-              title: 'More',
+              title: 'Build with',
               items: [
                 {
                   label: 'docusaurus',
                   href: 'https://docusaurus.io/docs/',
                 },
+              ],
+            },
+            {
+              title: 'Thanks',
+              items: [
                 {
-                  label: 'GitHub',
-                  href: 'https://github.com/facebook/docusaurus',
+                  label: 'TS Playground',
+                  href: 'https://www.typescriptlang.org/play',
                 },
               ],
             },
           ],
-          copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+          copyright: `Copyright © ${new Date().getFullYear()} ${title}.`,
         },
         prism: {
           theme: lightCodeTheme,
